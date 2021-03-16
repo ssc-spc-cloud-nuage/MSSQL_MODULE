bccb7d5813776eca7b63909b8e80df667898c971
Louis-Eric Tremblay <louis-eric.tremblay@canada.ca>
2021-03-16 14:47:13 -0400

    change mssql for sql..server

--- outputs.tf
+++ outputs.tf
@@ -7,16 +7,17 @@ output "name" {
 }
 
 output "administrator_login" {
-  value = azurerm_mssql_server.mssql.administrator_login
+  value = azurerm_sql_server.mssql.administrator_login
 }
 
 output "identity_tenant_id" {
-  value = azurerm_mssql_server.mssql.identity[0].tenant_id
+  value = azurerm_sql_server.mssql.identity[0].tenant_id
+  
 }
 
-# output "identity_object_id" {
-#   value = azurerm_mssql_server.mssql.identity[0].principal_id
-# }
+ output "identity_object_id" {
+   value = azurerm_sql_server.mssql.identity[0].principal_id
+}
 
 # Part of a hack for module-to-module dependencies.
 # https://github.com/hashicorp/terraform/issues/1178#issuecomment-449158607
