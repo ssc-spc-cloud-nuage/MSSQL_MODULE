fbc9a8a9fb6c7e7d23e67a27317f322431de95e5
Louis-Eric Tremblay <louis-eric.tremblay@canada.ca>
2021-04-01 14:40:23 -0400

    commit

--- storage.tf
+++ storage.tf
@@ -1,18 +1,18 @@
 # Storage Accounts
-resource "azurerm_storage_account" "mssql" {
+# resource "azurerm_storage_account" "mssql" {
 
-    for_each = [for s in var.server : {
-      name                          = "${var.environment}-cio-${s.sqlname}"
-      location                      = s.location
-      resource_group_name           = s.resource_group_name
-    }]
-    content {
-      name                          = "${var.environment}-cio-${server.value.sqlname}"
-      location                      = server.value.location
-      resource_group_name           = server.value.resource_group_name
+#     for_each = [for s in var.server : {
+#       name                          = "${var.environment}-cio-${s.sqlname}"
+#       location                      = s.location
+#       resource_group_name           = s.resource_group_name
+#     }]
+#     content {
+#       name                          = "${var.environment}-cio-${server.value.sqlname}"
+#       location                      = server.value.location
+#       resource_group_name           = server.value.resource_group_name
 
-    }   
-    account_tier              = "Standard"
-    account_replication_type  = "LRS"
+#     }   
+#     account_tier              = "Standard"
+#     account_replication_type  = "LRS"
 
-}
+# }
