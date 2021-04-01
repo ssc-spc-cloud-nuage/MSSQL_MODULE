daea8f45b3dad00b0192bf8cbbb406fe852c40cb
Louis-Eric Tremblay <louis-eric.tremblay@canada.ca>
2021-04-01 12:39:40 -0400

    commit

--- main.tf
+++ main.tf
@@ -8,99 +8,110 @@ terraform {
 }
 
 
-
 resource "azurerm_mssql_server" "mssql" {  
-  name                          = "${var.environment}-cio-${var.sqlname}"
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  version                       = var.mssql_version
-  administrator_login           = var.administrator_login
-  administrator_login_password  = var.administrator_login_password
-  public_network_access_enabled = false  
+   for_each = [for s in var.server : {
+    name                          = "${var.environment}-cio-${s.sqlname}"
+    location                      = s.location
+    resource_group_name           = s.resource_group_name
+    version                       = s.mssql_version
+    administrator_login           = s.administrator_login
+    administrator_login_password  = s.administrator_login_password
+    public_network_access_enabled = false
+    }]
+    content {
+      name                          = "${var.environment}-cio-${server.value.sqlname}"
+      location                      = server.value.location
+      resource_group_name           = server.value.resource_group_name
+      version                       = server.value.mssql_version
+      administrator_login           = server.value.administrator_login
+      administrator_login_password  = server.value.administrator_login_password
+      public_network_access_enabled = false
+    }    
+  
 
   azuread_administrator {
     login_username =   "louis-eric.tremblay@ssc-spc.gc.ca"
     tenant_id           = var.active_directory_administrator_tenant_id
     object_id           = var.active_directory_administrator_object_id
-  }
+  }  
 }
 
-resource "azurerm_mssql_database" "mssql" { 
-  #count = "${var.deploy == false ? 0 : 1}"         
-    name           = "${var.environment}-cio-${var.sqldbname}"
-    server_id      = azurerm_mssql_server.mssql.id
-    collation      = var.collation
-    license_type = "LicenseIncluded"
-    max_size_gb    = var.max_size_gb
-    read_scale     = var.read_scale
-    sku_name       = var.sku_name
-    zone_redundant = var.zone_redundant
+# resource "azurerm_mssql_database" "mssql" { 
+#   #count = "${var.deploy == false ? 0 : 1}"         
+#     name           = "${var.environment}-cio-${var.sqldbname}"
+#     server_id      = azurerm_mssql_server.mssql.id
+#     collation      = var.collation
+#     license_type = "LicenseIncluded"
+#     max_size_gb    = var.max_size_gb
+#     read_scale     = var.read_scale
+#     sku_name       = var.sku_name
+#     zone_redundant = var.zone_redundant
 
-    dynamic "short_term_retention_policy" {
-       for_each = var.policyretention_days == null ? [] : [var.policyretention_days]
-      content {
-        retention_days = var.policyretention_days
-      }     
-    }
+#     dynamic "short_term_retention_policy" {
+#        for_each = var.policyretention_days == null ? [] : [var.policyretention_days]
+#       content {
+#         retention_days = var.policyretention_days
+#       }     
+#     }
     
-    dynamic "long_term_retention_policy" {
-      for_each = var.week_of_year == null ? [] : [var.week_of_year]
-      content {
-        weekly_retention = var.weekly_retention
-        monthly_retention = var.monthly_retention
-        yearly_retention = var.yearly_retention
-        week_of_year =  var.week_of_year
-      }
-    }
-}
-
-resource   "azurerm_mssql_server_extended_auditing_policy" "mssql" { 
-  server_id                               = azurerm_mssql_server.mssql.id
-  storage_endpoint                        = azurerm_storage_account.mssql.primary_blob_endpoint
-  storage_account_access_key              = azurerm_storage_account.mssql.primary_access_key
-  storage_account_access_key_is_secondary = false
-  retention_in_days                       = 6
-} 
-
-resource "azurerm_mssql_server_security_alert_policy" "mssql" {  
-  resource_group_name        = var.resource_group_name
-  server_name                =  azurerm_mssql_server.mssql.name
-  state                      = "Enabled"
-  storage_endpoint           = azurerm_storage_account.mssql.primary_blob_endpoint
-  storage_account_access_key = azurerm_storage_account.mssql.primary_access_key
-  email_account_admins       = true
-  disabled_alerts = [    
-    "Data_Exfiltration"
-  ]
-  retention_days = 30
-}
-
-# resource "azurerm_sql_active_directory_administrator" "mssql" {  
-#   server_name         = azurerm_mssql_server.mssql.name
-#   resource_group_name = var.resource_group_name
-#   login               = "louis-eric.tremblay@ssc-spc.gc.ca"
-#   tenant_id           = var.active_directory_administrator_tenant_id
-#   object_id           = var.active_directory_administrator_object_id
+#     dynamic "long_term_retention_policy" {
+#       for_each = var.week_of_year == null ? [] : [var.week_of_year]
+#       content {
+#         weekly_retention = var.weekly_retention
+#         monthly_retention = var.monthly_retention
+#         yearly_retention = var.yearly_retention
+#         week_of_year =  var.week_of_year
+#       }
+#     }
 # }
 
-resource "azurerm_private_endpoint" "mssql" { 
-  name                = "${var.environment}-cio-${var.sqlname}-pe"
-  location            = var.location
-  resource_group_name = var.resource_group_name //"ScDc-CIO_APS_Network-rg"
-  subnet_id           = var.subnet_id
+# resource   "azurerm_mssql_server_extended_auditing_policy" "mssql" { 
+#   server_id                               = azurerm_mssql_server.mssql.id
+#   storage_endpoint                        = azurerm_storage_account.mssql.primary_blob_endpoint
+#   storage_account_access_key              = azurerm_storage_account.mssql.primary_access_key
+#   storage_account_access_key_is_secondary = false
+#   retention_in_days                       = 6
+# } 
 
-  private_dns_zone_group {
-    name = "${var.sqlname}privatednszonegroup"
-    private_dns_zone_ids = [var.DnsPrivatezoneId]
-  }
+# resource "azurerm_mssql_server_security_alert_policy" "mssql" {  
+#   resource_group_name        = var.resource_group_name
+#   server_name                =  azurerm_mssql_server.mssql.name
+#   state                      = "Enabled"
+#   storage_endpoint           = azurerm_storage_account.mssql.primary_blob_endpoint
+#   storage_account_access_key = azurerm_storage_account.mssql.primary_access_key
+#   email_account_admins       = true
+#   disabled_alerts = [    
+#     "Data_Exfiltration"
+#   ]
+#   retention_days = 30
+# }
 
-  private_service_connection {   
-    name                           = "${var.environment}-cio-${var.sqlname}-psc"
-    private_connection_resource_id =  azurerm_mssql_server.mssql.id
-    subresource_names              = ["sqlServer"]
-    is_manual_connection           = false
-  }
-}
+# # resource "azurerm_sql_active_directory_administrator" "mssql" {  
+# #   server_name         = azurerm_mssql_server.mssql.name
+# #   resource_group_name = var.resource_group_name
+# #   login               = "louis-eric.tremblay@ssc-spc.gc.ca"
+# #   tenant_id           = var.active_directory_administrator_tenant_id
+# #   object_id           = var.active_directory_administrator_object_id
+# # }
+
+# resource "azurerm_private_endpoint" "mssql" { 
+#   name                = "${var.environment}-cio-${var.sqlname}-pe"
+#   location            = var.location
+#   resource_group_name = var.resource_group_name //"ScDc-CIO_APS_Network-rg"
+#   subnet_id           = var.subnet_id
+
+#   private_dns_zone_group {
+#     name = "${var.sqlname}privatednszonegroup"
+#     private_dns_zone_ids = [var.DnsPrivatezoneId]
+#   }
+
+#   private_service_connection {   
+#     name                           = "${var.environment}-cio-${var.sqlname}-psc"
+#     private_connection_resource_id =  azurerm_mssql_server.mssql.id
+#     subresource_names              = ["sqlServer"]
+#     is_manual_connection           = false
+#   }
+# }
 
 # output "private_link_endpoint_ip03" {
 #   value = "${azurerm_private_endpoint.CIO-vcboardroom-pe.private_service_connection.0.private_ip_address}"
--- variables.tf
+++ variables.tf
@@ -1,69 +1,73 @@
 # Server
 
-variable "sqlname" {
+variable "server" {
   description = "The name of the MSSQL Server"
 }
 
-variable "sqldbname" {
-  description = "A mapping of database name and collation for the MSSQL database(s)"
-}
+# variable "sqlname" {
+#   description = "The name of the MSSQL Server"
+# }
+
+# variable "sqldbname" {
+#   description = "A mapping of database name and collation for the MSSQL database(s)"
+# }
 
 variable "dependencies" {
   type        = "list"
   description = "Dependency management of resources"
 }
 
-variable "administrator_login" {
-  description = "The Administrator Login for the MSSQL Server"
-}
+# variable "administrator_login" {
+#   description = "The Administrator Login for the MSSQL Server"
+# }
 
-variable "administrator_login_password" {
-  description = "The Password associated with the administrator_login for the MSSQL Server"
-}
+# variable "administrator_login_password" {
+#   description = "The Password associated with the administrator_login for the MSSQL Server"
+# }
 
-variable "retention_days" {
-  description = "Specifies the retention in days for logs for this MSSQL Server"
-  default     = 30
-}
+# variable "retention_days" {
+#   description = "Specifies the retention in days for logs for this MSSQL Server"
+#   default     = 30
+# }
 
-variable "sku_name" {
-  description = "Specifies the SKU Name for this MSSQL Server"
-  default     = "GP_Gen5_4"
-}
+# variable "sku_name" {
+#   description = "Specifies the SKU Name for this MSSQL Server"
+#   default     = "GP_Gen5_4"
+# }
 
-variable "mssql_version" {
-  description = "The version of the MSSQL Server"
-  default     = "12.0"
-}
+# variable "mssql_version" {
+#   description = "The version of the MSSQL Server"
+#   default     = "12.0"
+# }
 
-variable "max_size_gb" {
-  description = "Specifies the storage size for MSSQL to use"
-  default     = 64
-}
+# variable "max_size_gb" {
+#   description = "Specifies the storage size for MSSQL to use"
+#   default     = 64
+# }
 
-variable "zone_redundant" {
-  description = ""
+# variable "zone_redundant" {
+#   description = ""
 
-}
+# }
 
-variable "read_scale" {
-  description = ""
+# variable "read_scale" {
+#   description = ""
 
-}
+# }
 
-variable "collation" {
-  description = ""
+# variable "collation" {
+#   description = ""
 
-}
+# }
 
 variable "location" {
   description = "Specifies the supported Azure location where the resource exists"
   default     = "canadacentral"
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the MSSQL Server"
-}
+# variable "resource_group_name" {
+#   description = "The name of the resource group in which to create the MSSQL Server"
+# }
 
 variable "subnet_id" {
   description = "The ID of the subnet that the MSSQL server will be connected to"
@@ -107,21 +111,21 @@ variable "deploy" {
   default     = false
 }
 
-variable "weekly_retention" {
+# variable "weekly_retention" {
  
-}
-variable "monthly_retention" {
+# }
+# variable "monthly_retention" {
  
-}
-variable "yearly_retention" {
+# }
+# variable "yearly_retention" {
   
-}
-variable "week_of_year" {
+# }
+# variable "week_of_year" {
   
-}
+# }
 
 
-variable "policyretention_days" {
-  default     = 7
-}
+# variable "policyretention_days" {
+#   default     = 7
+# }
   
